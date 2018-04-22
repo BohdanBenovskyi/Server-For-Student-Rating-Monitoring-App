4bbb080b4776c37ba4f7c8d831a086b38776dd04
Bohdan Benovskyi <benovskiy1994@gmail.com>
2018-04-22 17:21:49 +0300

    fix connection close

--- src/com/benovskyi/bohdan/ServerSRM.java
+++ src/com/benovskyi/bohdan/ServerSRM.java
@@ -5,20 +5,30 @@ import java.sql.*;
 public class ServerSRM {
 
 	public static void main(String[] args) {
-	
 		try {
-			Connection myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/sysmon?autoReconnect=true&useSSL=false", "root", "2768652");
-			Statement myStmt = myConn.createStatement();
-			ResultSet myRs = myStmt.executeQuery("select * from users");
+			String url = "jdbc:mysql://localhost:3306/sysmon?autoReconnect=true&useSSL=false";
+			String login = "root";
+			String password = "2768652";
 			
-			while(myRs.next()) {
-				System.out.println(myRs.getString("iduser") + " " + myRs.getString("role") + " " 
-								+ myRs.getString("login") + " " + myRs.getString("password"));
+			Connection con = DriverManager.getConnection(url, login, password);
+			
+			try {
+				Statement myStmt = con.createStatement();
+				ResultSet myRs = myStmt.executeQuery("select * from users");
+				
+				while(myRs.next()) {
+					System.out.println(myRs.getString("iduser") + " " + myRs.getString("role") + " " 
+									+ myRs.getString("login") + " " + myRs.getString("password"));
+				}
+				
+				myRs.close();
+				myStmt.close();
+			} finally {
+				con.close();
 			}
-		}
-		catch(Exception ex) {
-			ex.printStackTrace();
-		}
+		} catch (Exception e) {
+            e.printStackTrace();
+        }
 		
 	}
 
